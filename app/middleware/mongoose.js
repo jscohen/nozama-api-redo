27ae25f7ceb3a3e6b51714aee8694970eab45d89
jscohen <jon.cohen08@gmail.com>
2017-06-01 15:08:15 -0400

    Added Mongoose Heroku URI

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -2,7 +2,7 @@
 
 const database = require('config/database')
 const mongoose = require('mongoose')
-const uri = process.env.MONGODB_URI || database.mongoose.uri
+const uri = process.env.MONGODB_URI || database.mongoose.uri || 'mongodb://immense-springs-27677.herokuapp.com/nozama-api-development'
 mongoose.Promise = global.Promise
 mongoose.connect(uri)
 
