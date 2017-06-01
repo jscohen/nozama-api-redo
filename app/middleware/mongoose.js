1ea7032ea12c6b2bf778642023bffab9438596f5
jscohen <jon.cohen08@gmail.com>
2017-06-01 16:23:43 -0400

    Testing again

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,16 +1,8 @@
 'use strict'
 
-const database = require('../../config/database')
+const database = require('config/database')
 const mongoose = require('mongoose')
-
-let uri = ''
-
-if (database === undefined) {
-  uri = 'mongodb://nozama-api-redo.herokuapp.com/nozama-api-development'
-} else {
-  uri = process.env.MONGODB_URI || database.mongoose.uri
-}
-
+const uri = process.env.MONGODB_URI || database.mongoose.uri
 mongoose.Promise = global.Promise
 mongoose.connect(uri)
 
