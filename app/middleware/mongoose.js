870c65210ecd1535a5944051f91f1766297b34f2
jscohen <jon.cohen08@gmail.com>
2017-06-01 15:30:10 -0400

    test

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -2,7 +2,7 @@
 
 const database = require('config/database')
 const mongoose = require('mongoose')
-const uri = process.env.MONGODB_URI || database.mongoose.uri
+const uri = process.env.MONGODB_URI || 'mongodb://nozama-api-redo.herokuapp.com/nozama-api-development' || database.mongoose.uri
 mongoose.Promise = global.Promise
 mongoose.connect(uri)
 
