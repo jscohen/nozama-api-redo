1997e20f2b076ab3a12dd3e48a144553e72a4105
jscohen <jon.cohen08@gmail.com>
2017-06-01 16:15:24 -0400

    Put in heroku URL for mongodb in URI variable in mongoose file

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -2,10 +2,16 @@
 
 const database = require('../../config/database')
 const mongoose = require('mongoose')
-const uri = process.env.MONGODB_URI || database.mongoose.uri || 'mongodb://nozama-api-redo.herokuapp.com/nozama-api-development'
+
+let uri = ''
+
+if (database.mongoose.uri === undefined) {
+  uri = 'mongodb://nozama-api-redo.herokuapp.com/nozama-api-development'
+} else {
+  uri = process.env.MONGODB_URI || database.mongoose.uri
+}
+
 mongoose.Promise = global.Promise
 mongoose.connect(uri)
-
-console.log(database.mongoose.uri)
 
 module.exports = mongoose
